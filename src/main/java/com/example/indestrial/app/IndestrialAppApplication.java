0afe497f8e788581dd780dfdce04711355fbda55
Zin Min Htet <zinminhtet.itwizard@gmail.com>
2026-03-27 16:01:02 +0630

    init

---
+++ src/main/java/com/example/indestrial/app/IndestrialAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.indestrial.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndestrialAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndestrialAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/indestrial/app/IndestrialAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.indestrial.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndestrialAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
